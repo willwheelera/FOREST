6d7659e5cec85e22d0fa31062cec73c08311da13
Will <willwheelera@gmail.com>
2025-05-15 11:10:43 -0400

    small updates on mean_distribution, plot max

--- timeseries_models/mean_distribution.py
+++ timeseries_models/mean_distribution.py
@@ -14,26 +14,40 @@ def run():
     #fname = "../data/Alburgh/2024-01-01_2024-12-31_South_Alburgh_Load_corrected.parquet"
     #timeseries_df = pd.read_parquet(fname)[:-1]  # index: 'timestamp', columns: 'asset_id', data: 'value'
     #timeseries_df[timeseries_df.columns[:1000]].to_parquet("test_sample1000.parquet")
-    df = pd.read_parquet("test_sample1000.parquet")
-    meterdf, keys = get_meter_data()
+    df = pd.read_parquet("test_sample100.parquet")
+    meterdf = None if len(df) == 100 else get_meter_data()[0]
     df = clean_data.clean_all(df, meterdf)
     df = clean_data.remove_all_devices(df, meterdf)
     data = df.values
 
     #data = data / np.abs(np.amax(data, axis=-1, keepdims=True))
     #data = data / np.abs(np.std(data, axis=-1, keepdims=True))
-    plot_mean(data)
+    plot_max(data)
+
+def plot_max(v):
+    # divide by mean - if the load curves scale, this helps compare
+    # if the load curves are different sources added together, it's bad
+    # Do we want a vector space where vectors all have the same sum? Maybe the same length (norm)? Probably not.....
+    # how do we tell?
+    #   PCA is components added together. but scaled anyway
+    #   how much does the max vary?
+    m = v.mean(axis=0)
+    v = v - m
+    _max = np.amax(v, axis=0) # this is max over the year... not that useful
+    plt.scatter(m, _max) # is there a correlation between mean and spikiness?
+    plt.show()
+    
 
 def plot_mean(v):
+    n = len(mean)
     mean = v.mean(axis=0) # avg over year
-    hist, bins = np.histogram(mean, bins=100, density=True)
+    hist, bins = np.histogram(mean, bins=n//10, density=True)
     ch = np.cumsum(hist)
     plt.plot(bins[:-1], ch * np.diff(bins))
 
-    n = len(mean)
     print("len mean", n)
     m = np.sort(mean)
-    smoothm = smooth(m, 15)
+    smoothm = smooth(m, 5)
     plt.plot(m, np.linspace(0, 1, n))
     plt.plot(smoothm, np.linspace(0, 1, n))
 
@@ -41,7 +55,7 @@ def plot_mean(v):
     plt.plot(bins[:-1], smooth(hist))
     plt.title("distribution across meters")
     plt.xlabel("mean load over 2024")
-    plt.savefig("mean_distribution.pdf", bbox_inches="tight")
+    #plt.savefig("mean_distribution.pdf", bbox_inches="tight")
     plt.show()
 
 def smooth(x, n=3):
