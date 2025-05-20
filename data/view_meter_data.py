48791ccf548c979c70e8fd486da07140ad00cd18
Will <willwheelera@gmail.com>
2025-05-20 15:39:26 -0400

    add print columns

--- data/view_meter_data.py
+++ data/view_meter_data.py
@@ -3,6 +3,7 @@ import sys
 
 meters = pd.read_parquet("VEC_meter_number_data.parquet")
 gens = pd.read_parquet("VEC_gen_meter_number_data.parquet")
+print(meters.columns)
 
 check_non_solar = [3078906, 39608, 3081194, 3093019]
 isin = gens["Service Number"].isin(check_non_solar)
