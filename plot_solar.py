992d30a9091ed66434a66ae80a4ab01f6b58d045
Will Wheeler <willwheelera@gmail.com>
2025-05-21 10:49:37 -0400

    add plot_solar histogram plot

--- plot_solar.py
+++ plot_solar.py
@@ -3,10 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+TOWNS = ["ALBURGH", "SOUTH HERO", "GLOVER"]
+
 def run():
     fname = 'data/PP12 Jan2025.xlsx'
     df = pd.read_excel(fname)
     df = clean_pv_data(df)
+    add_pending(df)
+    print_total_nums(df)
+    plot(df)
 
 # Clean up PV spreadsheet
 def clean_pv_data(df):
@@ -17,14 +23,12 @@ def clean_pv_data(df):
         select = df["City/Town"] == k
         df.loc[select, "City/Town"] = v
 
-    towns = ["ALBURGH", "SOUTH HERO", "GLOVER"]
-    df = df[df["City/Town"].isin(towns)]
     return df
 
 def add_pending(df):
     print(df["Status"].unique())
-    print("n pending", (df["Status"] =="Pending").sum())
-    pending = df["Status"] =="Pending"
+    print("n pending", (df["Status"] == "Pending").sum())
+    pending = df["Status"] == "Pending"
     for i in df.index[pending]:
         df.loc[i, "In-Service Date"] = pd.Timestamp(2025, 6, 1)
 
@@ -33,13 +37,29 @@ def print_total_nums(df):
     meterdf = pd.read_parquet(metername)
 
     for sub, name in zip([28, 29, 43], ["ALBURGH", "SOUTH HERO", "GLOVER"]):
-        tmp = df[df["City/Town"]==name]
-        print("Substation", sub, f"{name:<10}", (tmp["Status"]!= "Cancelled").sum(), end=" / ")
-    print((meterdf["Substation"]==str(sub)).sum())
+        tmp = df[df["City/Town"] == name]
+        n_pv = (tmp["Status"] != "Cancelled").sum()
+        nmeter = (meterdf["Substation"] == str(sub)).sum()
+        print(f"Substation {sub} {name:<10} {n_pv} / {nmeter}")
 
 def plot(df):
+    #df = df[df["City/Town"].isin(TOWNS)]
     df = df[~df["In-Service Date"].isna()]
     df = df.sort_values(by="In-Service Date")
 
-    sns.kdeplot(data=df, hue="City/Town", x="In-Service Date", bw_adjust=0.1)
+    sns.histplot(
+        data=df,
+        #hue="City/Town",
+        x="In-Service Date",
+        binwidth=365,
+        multiple="dodge",
+        shrink=0.8,
+        kde=True,
+        kde_kws=dict(bw_adjust=0.5),
+    )
+    plt.savefig("figures/solar_growth_total.pdf", bbox_inches="tight")
     plt.show()
+
+
+if __name__ == "__main__":
+    run()
