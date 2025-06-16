59c2f8c04fd3b49ab5b08496415f5f349894b4fe
Will Wheeler <willwheelera@gmail.com>
2025-06-16 16:18:14 -0400

    update extract_transformer_rating to include X, Y

--- extract_transformer_rating.py
+++ extract_transformer_rating.py
@@ -9,7 +9,9 @@ pkl_file = f"data/{substation_name}/{substation_name}_Model.pkl"
 with open(pkl_file, "rb") as file:
     ps_model = pickle.load(file)
 
-vals = [(b.name, b.ratedKVA) for i, b in enumerate(ps_model.Branches) if b.type=="transformer"]
-tf_name, ratedKVA = list(zip(*vals))
-df = pd.DataFrame({"tf_name": tf_name, "ratedKVA": ratedKVA})
-df.to_csv("transformer_ratings.csv")
+vals = [(b.name, b.ratedKVA, b.X_coord, b.Y_coord) for i, b in enumerate(ps_model.Branches) if b.type=="transformer"]
+tf_name, ratedKVA, x, y = list(zip(*vals))
+df = pd.DataFrame({"tf_name": tf_name, "ratedKVA": ratedKVA, "x": x, "y": y})
+df.to_csv(f"data/{substation_name}/transformer_ratings.csv")
+
+# need to check gs_tran_kva_a, etc - what happens if two or three of (a, b, c) are present? how many gs_rated_kva don't match?
