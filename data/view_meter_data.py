c7d9efdc38fe310adf57feb0ecd40c2496d17809
Will <willwheelera@gmail.com>
2025-06-20 15:28:40 -0400

    print substation meter counts in view_meter_data

--- data/view_meter_data.py
+++ data/view_meter_data.py
@@ -1,9 +1,17 @@
 import pandas as pd
 import sys
+import numpy as np
 
 meters = pd.read_parquet("VEC_meter_number_data.parquet")
 gens = pd.read_parquet("VEC_gen_meter_number_data.parquet")
 print(meters.columns)
+
+s = meters["Substation"]
+s = s[~s.isna()]
+vals, counts = np.unique(s, return_counts=True)
+for v, c in zip(vals, counts):
+    print(v, c)
+quit()
 
 check_non_solar = [3078906, 39608, 3081194, 3093019]
 isin = gens["Service Number"].isin(check_non_solar)
