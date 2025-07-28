1bd0d7c2d7eecba61365e077faa559a5b9085fe0
Will <willwheelera@gmail.com>
2025-07-28 17:07:37 -0400

    workflow.py is working

--- workflow.py
+++ workflow.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import scipy
+import scipy.sparse
 import time
 import json
 import pickle
@@ -10,6 +10,8 @@ import sun_model
 import loads_to_transformers
 import transformer_aging
 import device_data
+import read_in_data
+from timer import Timer
 
 
 # General outline
@@ -21,12 +23,13 @@ def run_instance(nyears=20, year0=2025):
     solardf: dataframe with meter numbers and solar status
     mapfile: information about which meters connect to which transformers
     """
-    t0 = time.perf_counter()
-    print("Starting")
+    timer = Timer(30)
+    timer.print("Starting")
     # Read in load data -- may want to use raw data instead
     fname = "data/Alburgh/2024-01-01_2024-12-31_South_Alburgh_Load_corrected.parquet"
     #mapfile = "data/Alburgh/transformer_load_map.json" # map meters to transformers
     mapfile = "data/Alburgh/transformer_map.pkl" # map meters to transformers
+
     Ldata = pd.read_parquet(fname)
     meterdf, keys = device_data.get_meter_data("sub28")
     #meterdf = meterdf[meterdf["Substation"] == 28]
@@ -39,6 +42,7 @@ def run_instance(nyears=20, year0=2025):
     Hsize, Esize, Ssize = np.zeros(Ldata.shape[1]), np.zeros(Ldata.shape[1]), np.zeros(Ldata.shape[1])
 
     age_curves = []
+    load_curves = []
     if mapfile.endswith("json"):
         with open(mapfile, "r") as f:
             load_map = json.load(f)
@@ -46,54 +50,66 @@ def run_instance(nyears=20, year0=2025):
     else:
         with open(mapfile, "rb") as f:
             load_map = pickle.load(f)["load_map"]
-        load_map = [l for l in load_map if l[0] in Ldata.columns]
-        age_offset = np.zeros(len(np.unique([m[1] for m in load_map])))
-        # TODO try sparse matrix version # load_map_sparse = scipy.sparse.coo_array((np.ones(len(load_map)), np.array(load_map).T))
+        #load_map = [l for l in load_map if l[0] in Ldata.columns]
+        m2t_map = loads_to_transformers.meter_to_transformer_matrix(load_map, Ldata.columns)
+        age_offset = np.zeros(m2t_map.shape[1])
+    tf_ratings = read_in_data.read_transformer_ratings("Alburgh")
+    tf_ratings = tf_ratings.loc[m2t_map.columns]
+    TF_RATINGS = tf_ratings["ratedKVA"].values
 
-    print("data loaded", time.perf_counter() - t0)
+    timer.print("data loaded")#, time.perf_counter() - t0)
     T0 = 110
     for year in np.arange(nyears) + year0:
+        timer.reset()#t0 = time.perf_counter()
+        timer.print(f"Reset timer {year}")
         H_g = placeholders.growth_rate_heatpumps(year)
         E_g = placeholders.growth_rate_evs(year)
         S_g = placeholders.growth_rate_solar(year)
-        print(f"{year} growth rates computed", time.perf_counter() - t0)
+        print(f"growth rates: H {H_g}, E {E_g}, S {S_g}")
 
         adoptH = placeholders.adopt_heatpumps(Ldata, meterdf, H_g)
         adoptE = placeholders.adopt_evs(Ldata, meterdf, E_g)
         adoptS = placeholders.adopt_solar(Ldata, solardf, S_g)
-        print(f"adoption computed", time.perf_counter() - t0)
+        print(f"adopted rates: H {adoptH.sum()}, E {adoptE.sum()}, S {adoptS.sum()}")
         Hsize += placeholders.size_heatpumps(Ldata, adoptH).values
         Esize += placeholders.size_evs(Ldata, adoptE).values
-        Ssize += placeholders.size_solar(Ldata, adoptS).values
-        print(f"sizes updated", time.perf_counter() - t0)
+        Ssize += -placeholders.size_solar(Ldata, adoptS).values
 
         weather = weather_data.load_data.generate()
-
 
         LH = placeholders.generate_heatpump_load_profile(weather)[:, np.newaxis] # just one profile
         LE = placeholders.generate_ev_load_profile(Esize)
         LS = sun_model.generate()[:, np.newaxis] # just one profile
         L0 = placeholders.generate_background_profile(Ldata)
         L = Hsize*LH + LE + Ssize*LS + L0
-        print(f"profiles generated", time.perf_counter() - t0)
+        timer.print(f"meter loads calculated")#, time.perf_counter() - t0)
 
         # Transformer loads
-        L_tr = loads_to_transformers.meters_to_transformers_tuples(load_map, L).values
-        #L_tr = L.values @ load_map_sparse
-        print(f"transformer loads calculated", time.perf_counter() - t0)
+        #L_tr = loads_to_transformers.meters_to_transformers_tuples(load_map, L).values
+        #L_tr = m2t_mapper.meters_to_transformers(L)
+        L_tr = L.values @ m2t_map.values
+        L_tr = (L_tr / TF_RATINGS)
+        load_curves.append(L_tr)
+        timer.print(f"transformer loads calculated")#, time.perf_counter() - t0)
 
         # Transformer aging
         hotspot = transformer_aging.temperature_equations(L_tr, weather, T0=T0)
-        print(f"hotspot T calculated", time.perf_counter() - t0)
         T0 = hotspot[-1] # for iterating multiple years if desired
         aging = transformer_aging.effective_aging(hotspot)
+        timer.print(f"aging calculated")#, time.perf_counter() - t0)
         aging += age_offset
         age_offset = aging[-1]
         age_curves.append(aging)
-        print(f"year {year} done", time.perf_counter() - t0)
+        timer.print(f"year {year} done")#, time.perf_counter() - t0)
         
     full_age_curve = np.concatenate(age_curves, axis=0)
+    df = pd.DataFrame(data=full_age_curve, columns=m2t_map.columns)
+    df.to_parquet(f"output/alburgh_tf_aging_{year0}_{nyears}years.parquet")
+    full_load_curve = np.concatenate(load_curves, axis=0)
+    df = pd.DataFrame(data=full_load_curve, columns=m2t_map.columns)
+    df.to_parquet(f"output/alburgh_tf_load_{year0}_{nyears}years.parquet")
+    # TODO save to hdf instead of multiple parquet files
     
 if __name__ == "__main__":
     
-    run_instance(nyears=2, year0=2025)
+    run_instance(nyears=10, year0=2025)
