8ca32fa712de6ff134cb22de9db8c9a05ab5378b
Will <willwheelera@gmail.com>
2025-09-09 13:43:43 -0400

    clean up ev_charging_model

--- ev_charging_model.py
+++ ev_charging_model.py
@@ -1,53 +1,68 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # Weibull model for daily miles (km) driven, http://dx.doi.org/10.1016/j.trb.2017.04.008
+# Ploetz et al. 2017 
 # f(r) = (beta/tau) (r/tau)^{beta-1} e^{-(r/tau)^beta}
 
+# Parameters
+# Random parameters drawn uniformly between lims
+BATTERY_LIMS = (50., 120.) # EV size kWh
+TAU_LIMS = (40., 60.) # ~avg daily km (~10% over because of beta)
+MPKWH_LIMS = (2., 5.) # vehicle efficiency miles/kWh
+RECHARGE_LIMS = (0.4, 0.6) # when to recharge, % battery used
 
-def generate_ev_load_profile(Esize, seed=1, charging_rate=7.2, ndays=365):
-    Esize = np.asarray(Esize)
-    hasEV = Esize > 0
-    n = hasEV.sum()
+# Fixed parameters
+BETA = 1.53 # Weibull shape parameter for miles driven
+EFF = 0.85 # charging efficiency
+INIT_BATTERY = 0.3 # initial battery used (1 - SOC) at day 0
+CHARGING_RATE = 7.2 # kW, level 2 charging rate
+
+# Gaussian parameters
+PLUGIN = (6.5, 1.5) # mean (PM), std (h) of plug-in time
+
+def generate_parameters(inds, seed=1):
+    n = len(inds)
     rng = np.random.default_rng(seed)
-    daily_load = generate_daily_charging_load(Esize, ndays, seed)
-    load_profile = np.zeros((len(Esize), ndays, 24))
-    for ev, load in zip(np.where(hasEV)[0], daily_load.T):
+    battery = rng.uniform(*BATTERY_LIMS, size=n) 
+    tau = rng.uniform(*TAU_LIMS, size=n) / 1.609 #  km -> miles 
+    mpkWh = rng.uniform(*MPKWH_LIMS, size=n) 
+    ch_at = rng.uniform(*RECHARGE_LIMS, size=n) 
+    scale = tau / mpkWh / battery
+    parameters = np.stack([inds, battery, scale, ch_at], axis=0)
+    return parameters
+
+def generate_ev_load_profile(Eparams, n, seed=1, ndays=365):
+    daily_load = generate_daily_charging_load(Eparams, ndays, seed)
+    inds = Eparams[0].astype(np.int64)
+    load_profile = np.zeros((n, ndays* 24)) # 24 h/day, fixed
+    rng = np.random.default_rng(seed)
+    tmp = np.zeros((ndays, 24))
+    for ev, load in zip(inds, daily_load.T):
         days = np.where(load > 0)[0]
         n_ = len(days)
-        start_time = np.floor(rng.normal(6.5, 1.5, size=n_)).astype(np.int64)
-        duration = np.ceil(load[days] / charging_rate).astype(np.int64)
+        start_time = np.floor(rng.normal(*PLUGIN, size=n_)).astype(np.int64)
+        duration = np.ceil(load[days] / CHARGING_RATE).astype(np.int64)
+        tmp[:] = 0.
         for d, s, t in zip(days, start_time, duration):
-            load_profile[ev, d, s:s+t] = charging_rate
-    return np.roll(load_profile.reshape(-1, ndays*24), 12, axis=1)
+            tmp[d, s:s+t] = CHARGING_RATE
+        load_profile[ev] = np.roll(tmp.reshape(-1), 12)
+    return load_profile.T
 
-# Want to calculate charge depleted
-# Parameters: 
-#   car efficiency miles/kWh (2-5)
-#   charge threshold (40-60) # when to recharge
-#   tau (30-55) # avg miles driven
-#   beta 1.53 #(1.4-1.7)
-#   battery size
-#   charging efficiency 85%
-def generate_daily_charging_load(battery_size, ndays, seed=1):
-    battery_size = battery_size[battery_size > 0]
-    n = len(battery_size)
+def generate_daily_charging_load(params, ndays, seed=1):
+    _, battery_size, scale, charge_threshold = params
+    n = params.shape[1]
     rng = np.random.default_rng(seed)
-    tau = rng.uniform(30, 55, size=n) # tau and mpkWh don't really need to be separated
-    mpkWh = rng.uniform(2, 5, size=n)
-    scale = tau / mpkWh / battery_size
-    beta = 1.53 # 
-    charge_threshold = rng.uniform(0.4, 0.6, size=n) # how much battery drain before charging again
-    eff = 0.85
     
-    charge_used = rng.weibull(beta, size=(ndays, n)) * scale
+    charge_used = rng.weibull(BETA, size=(ndays, n)) * scale
     load = np.zeros((ndays, n))
-    cum = np.zeros(n) + 0.3 # start off with partial battery
+    cum = np.zeros(n) + INIT_BATTERY # start off with partial battery
     for t, c in enumerate(charge_used):
         cum += c
         cum = np.minimum(cum, 1.)
         recharge = cum > charge_threshold
-        load[t, recharge] = cum[recharge] * battery_size[recharge] / eff
+        load[t, recharge] = cum[recharge] * battery_size[recharge] / EFF
         cum[recharge] = 0.
     
     return load
@@ -55,7 +70,6 @@ def generate_daily_charging_load(battery_size, ndays, seed=1):
 
 def plot_distribution():
     tau = 1#42.6
-    beta = 1.53
     for beta in np.linspace(1.4, 1.7, 5):
         f = lambda r: (beta/tau) *(r/tau)**(beta-1) * np.exp(-(r/tau)**beta)
         d = np.linspace(0, 2.5*tau, 100)
@@ -64,18 +78,17 @@ def plot_distribution():
     plt.show()
 
 if __name__ == "__main__":
-    Esize = np.array([50, 60, 70, 80, 90])
-    n = len(Esize)
+    n = 5
+    inds = np.arange(n)
+    params = generate_parameters(inds, seed=1)
     ndays = 7
-    load = generate_ev_load_profile(Esize, seed=1, charging_rate=7.2, ndays=ndays)
-    daily_load = generate_daily_charging_load(Esize, ndays, seed=1)
+    load = generate_ev_load_profile(params, n, seed=1, ndays=ndays)
+    daily_load = generate_daily_charging_load(params, ndays, seed=1)
 
+    scale = params[2]
+    charge_threshold = params[3]
     rng = np.random.default_rng(1)
-    tau = rng.uniform(30, 55, size=n) # tau and mpkWh don't really need to be separated
-    mpkWh = rng.uniform(2, 5, size=n)
-    scale = tau / mpkWh / np.asarray(Esize)
-    charge_threshold = rng.uniform(0.4, 0.6, size=n) # how much battery drain before charging again
-    charge_used = np.cumsum(rng.weibull(1.53, size=(ndays, n)) * scale, axis=0) + 0.3
+    charge_used = np.cumsum(rng.weibull(BETA, size=(ndays, n)) * scale, axis=0) + INIT_BATTERY
 
     fig, axs = plt.subplots(3, 1)
     t = np.arange(ndays*24) / 24
