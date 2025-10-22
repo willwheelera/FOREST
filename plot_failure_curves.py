73f7de8860229151239ef313abfb40588c8a1e7e
Will <willwheelera@gmail.com>
2025-10-22 17:25:29 -0400

    convert plot_failure_curves to a function and plot all scenarios on subplots

--- plot_failure_curves.py
+++ plot_failure_curves.py
@@ -1,82 +1,97 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 from visualize_network import visualize_network
 import pickle
 import loads_to_transformers
 import read_in_data
 import sys
+matplotlib.rcParams["font.size"] = 7
+matplotlib.rcParams["font.family"] = "serif"
 
 FIGSIZE = (3, 3)
 year0 = 2025
 nyears = 20
-GROWTH = "MH"
-if len(sys.argv) > 1:
-    GROWTH = sys.argv[1]
-    assert GROWTH in ["MED", "MH", "HIGH", "base150MED", "base150MH", "base150HIGH", "tran150MED", "tran150MH", "tran150HIGH"]
-failure_curves = pd.read_parquet(f"output/alburgh_tf_failure_curves_{GROWTH}_{year0}_{nyears}years.parquet")
-fname = "data/Alburgh/2024-01-01_2024-12-31_South_Alburgh_Load_corrected.parquet"
-mapfile = "data/Alburgh/transformer_map.pkl" # map meters to transformers
-#Ldata = pd.read_parquet(fname)
-#with open(mapfile, "rb") as f:
-#    load_map = pickle.load(f)["load_map"]
-#m2t_map = loads_to_transformers.meter_to_transformer_matrix(load_map, Ldata.columns)
-m2t_map = pd.read_parquet("data/Alburgh/transformer_map_matrix.parquet")
-tf_devices = pd.read_parquet(f"output/alburgh_tf_devices_{GROWTH}_{year0}_{nyears}years_avg.parquet")
-tf_ratings = read_in_data.read_transformer_ratings("Alburgh")
 
-final_prob = failure_curves.iloc[-1]
-inds = np.argsort(final_prob)
-nmeters = m2t_map.sum(axis=0)[final_prob.index]
+def run(basename):
 
-select = inds[-25:]
+    fname = "data/Alburgh/2024-01-01_2024-12-31_South_Alburgh_Load_corrected.parquet"
+    mapfile = "data/Alburgh/transformer_map.pkl" # map meters to transformers
+    #Ldata = pd.read_parquet(fname)
+    #with open(mapfile, "rb") as f:
+    #    load_map = pickle.load(f)["load_map"]
+    #m2t_map = loads_to_transformers.meter_to_transformer_matrix(load_map, Ldata.columns)
+    m2t_map = pd.read_parquet("data/Alburgh/transformer_map_matrix.parquet")
+    tf_ratings = read_in_data.read_transformer_ratings("Alburgh")
 
-info = failure_curves.iloc[-1, select]
-#print(info)
-isort = tf_devices.index.sort_values()[-10:]
-#print(tf_devices.loc["tran_87721"])
-#print(tf_ratings.loc[isort])
-tf_devices["nmeters"] = nmeters#[info.index]
-print(m2t_map.shape)
-not_tran = ~tf_devices.index.str.startswith("tran_")
-tmp = tf_devices[not_tran]
-print("adoption level")
-#print(tmp[["nmeters", "hasH", "hasE", "hasS"]].sum(axis=0))
-print(tmp[["nmeters", "hasH", "hasE", "hasS"]].sum(axis=0) / m2t_map.shape[0])
+    fig, axs = plt.subplots(1, 5, figsize=(9, 2))
 
-tf_devices = tf_devices.loc[info.index]
-tf_devices["p_fail"] = info
-tf_devices["ratedKVA"] = tf_ratings.loc[info.index, "ratedKVA"]
-tf_devices["id"] = tf_ratings.loc[info.index, "id"]
-tf_devices = tf_devices[["id", "p_fail", "nmeters", "ratedKVA", "H", "E", "S", "hasH", "hasE", "hasS"]]
-print(tf_devices.round(3))
+    for i, GROWTH in enumerate(["LOW", "MED", "HIGH", "XMH"]):
+        g_name = dict(LOW="MED", MED="MH", HIGH="HIGH", XMH="MH")[GROWTH]
+        tag = f"{basename}{g_name}_{year0}_{nyears}years"
+        xtag = "_1-1000" if GROWTH=="XMH" else ""
+        tf_devices = pd.read_parquet(f"output/alburgh_tf_devices_{tag}_avg{xtag}.parquet")
+        failure_curves = pd.read_parquet(f"output/alburgh_tf_failure_curves_{tag}{xtag}.parquet")
 
+        ############################################
 
-for seed in []:#, 5, 6, 7, 8, 9]:
-    tmpdf = pd.read_parquet(f"output/alburgh_tf_devices_{GROWTH}_{year0}_{nyears}years_{seed}.parquet")
-    print(tmpdf.loc[info.index])
+        final_prob = failure_curves.iloc[-1]
+        inds = np.argsort(final_prob)
+        nmeters = m2t_map.sum(axis=0)[final_prob.index]
 
-select_curves = failure_curves.values[:, select]
+        select = inds[-25:]
 
-plt.figure(figsize=FIGSIZE)
-plt.plot(final_prob.values[inds], label="p_fail")
-#plt.plot(nmeters.values[inds]/10, label="nmeters/10")
-plt.axvline(x=len(inds)-len(select), lw=0.4, c="k")
-#plt.axhline(y=0.05, lw=0.4, c="k")
-plt.xlabel("transformer index")
-plt.ylabel("final failure probability")
-plt.savefig(f"figures/alburgh_tf_failure_final_{GROWTH}_{year0}_{nyears}years.pdf", bbox_inches="tight")
+        info = failure_curves.iloc[-1, select]
+        isort = tf_devices.index.sort_values()[-10:]
+        tf_devices["nmeters"] = nmeters#[info.index]
+        not_tran = ~tf_devices.index.str.startswith("tran_")
+        tmp = tf_devices[not_tran]
+        print(m2t_map.shape)
+        print("adoption level")
+        print(tmp[["nmeters", "hasH", "hasE", "hasS"]].sum(axis=0) / m2t_map.shape[0])
 
-plt.figure(figsize=FIGSIZE)
-t = np.arange(len(failure_curves)) / 8760
-plt.plot(t[::24], select_curves[::24])
-plt.ylim([-0.04, 1.0])
-plt.xlabel("year")
-plt.ylabel("failure probability")
-plt.title("Average of 100 runs")
-plt.savefig(f"figures/alburgh_tf_failure_{GROWTH}_{year0}_{nyears}years.pdf", bbox_inches="tight")
+        ############################################
 
-visualize_network(xfmrs=failure_curves.columns[select])
+        tf_devices = tf_devices.loc[info.index]
+        tf_devices["p_fail"] = info
+        tf_devices["ratedKVA"] = tf_ratings.loc[info.index, "ratedKVA"]
+        tf_devices["id"] = tf_ratings.loc[info.index, "id"]
+        tf_devices = tf_devices[["id", "p_fail", "nmeters", "ratedKVA", "H", "E", "S", "hasH", "hasE", "hasS"]]
+        #print(tf_devices.round(3))
 
-plt.show()
+        select_curves = failure_curves.values[:, select]
 
+        #plt.figure(figsize=FIGSIZE)
+        nhide = 800
+        t = np.arange(nhide, len(final_prob))
+        axs[0].plot(t, final_prob.values[inds][nhide:], label=GROWTH+xtag)
+        #plt.savefig(f"figures/alburgh_tf_failure_final_{GROWTH}_{year0}_{nyears}years.pdf", bbox_inches="tight")
+
+        #plt.figure(figsize=FIGSIZE)
+        a = "bcde"[i]
+        t = np.arange(len(failure_curves)) / 8760
+        axs[i+1].plot(t[::24], select_curves[::24], c="k", lw=0.5)
+        axs[i+1].set_ylim([-0.04, 1.0])
+        axs[i+1].set_xlabel("Year")
+        axs[i+1].set_yticklabels([])
+        axs[i+1].tick_params(axis='y', which='major', length=0)
+        axs[i+1].set_title(f"({a})", weight="bold", fontsize="medium", loc="left")
+        axs[i+1].grid(axis="y", lw=0.5, c="lightgray")
+        #plt.savefig(f"figures/alburgh_tf_failure_{GROWTH}_{year0}_{nyears}years.pdf", bbox_inches="tight")
+
+        #visualize_network(xfmrs=failure_curves.columns[select])
+
+    axs[0].grid(axis="y", lw=0.5, c="lightgray")
+    axs[0].axvline(x=len(inds)-len(select), lw=0.5, c="k")
+    axs[0].set_xlabel("Transformer index")
+    axs[0].set_ylabel("Failure probability")
+    axs[0].set_title("(a)", weight="bold", fontsize="medium", loc="left")
+    axs[0].legend()
+    plt.tight_layout()
+    #plt.savefig(f"figures/alburgh_tf_failure_{basename}_{year0}_{nyears}years.pdf", bbox_inches="tight")
+    plt.show()
+
+if __name__ == "__main__":
+    for basename in [""]:#, "base150", "tran150"]:
+        run(basename)
